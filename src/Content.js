f2c1b7ff7caa324439a170b124167de6cad751a8
VasiliyTroshin <basil-v@mail.ru>
2022-03-07 08:52:32 +1000

    дз1 gj

---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Content = () => {
+  return (
+    <div>
+      <ul>
+          <li>Первый проект</li>
+          <li>Первый урок</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Content
