8facdec4a02e1040cd7ebe85c80b3372edf5c61f
VasiliyTroshin <basil-v@mail.ru>
2022-03-15 16:37:11 +1000

    Домашнее задание№2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Content from './Content';
 import Message from './Message';
 import './App.scss'
 
+const AUTHOR={
+  me:'me',
+  bot:'bot'
+}
 
 function App() {
+ const[messageList,setMessageList]=useState([]);
+ const[value,setValue]=useState('');
+
+const handleButton=()=>{
+  setMessageList([...messageList,{text:value,author:AUTHOR.me}]);
+  setValue('');
+}
+
+const handleChange=(event)=>{
+    const valueFromInput=event.target.value;
+    setValue(valueFromInput);
+}
+
+useEffect(()=>{
+  if(messageList.length>0 && messageList[messageList.length-1].author===AUTHOR.me){
+    setMessageList([...messageList,{text:'Hello,I am bot',author:AUTHOR.bot}])
+  }
+},
+[messageList]
+)
+
   return (
-    <div className="App">
-     <h1>Первое приложение</h1>
-    <img className='imgEl' src={'https://image.pngaaa.com/930/2507930-middle.png'}></img>
-    <Content />
-    <Message text='React — это инструмент для создания пользовательских интерфейсов' />
+    <div >
+   <div className="App">
+        <div className='messageLs'>
+          {messageList.map((message, index) => (<div className={`messageDesign ${message.author==='me'?'me':'bot'}`}>
+            {message.text} <sup>{message.author}</sup></div>))}
+        </div>
+        <div >
+          <input onChange={handleChange} value={value} />
+          <button onClick={handleButton} className='btn'>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
