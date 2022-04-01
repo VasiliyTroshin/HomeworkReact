0227c66d4600d955501920ef5b3dc097e76a5baf
VasiliyTroshin <basil-v@mail.ru>
2022-04-01 23:23:21 +1000

    Домашнее задание №4

--- src/App.js
+++ src/App.js
@@ -2,89 +2,29 @@ import React, { useEffect, useState } from 'react';
 import Content from './Content';
 import Message from './Message';
 import './App.scss'
-import { TextField,Button } from '@mui/material'
-import SendIcon from '@mui/icons-material/Send'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemText from '@mui/material/ListItemText'
-import CommentIcon from '@mui/icons-material/Comment'
-import IconButton from '@mui/material/IconButton'
+import {Route,Routes,Link} from 'react-router-dom'
+import Home from './pages/Home';
+import Chats from './pages/Chats';
+import Profile from './pages/Profile';
+import NoChats from './pages/NoChats';
+import Layout from './pages/Layout';
 
-const AUTHOR={
-  me:'me',
-  bot:'bot'
-}
+
 
 function App() {
- const[messageList,setMessageList]=useState([]);
- const[value,setValue]=useState('');
- 
-
-const handleButton=()=>{
-  setMessageList([...messageList,{text:value,author:AUTHOR.me}]);
-  setValue('');
-}
-
-const handleChange=(event)=>{
-    const valueFromInput=event.target.value;
-    setValue(valueFromInput);
-}
-
-useEffect(()=>{
-  if(messageList.length>0 && messageList[messageList.length-1].author===AUTHOR.me){
-    setMessageList([...messageList,{text:'Hello,I am bot',author:AUTHOR.bot}])
-  }
-},
-[messageList]
-)
-
-
 
   return (
-    <div>
-   <div className="App">
-      <div className='list'>
-
-          <List sx={{ width: '100px', maxWidth: 200, bgcolor: 'gbackground.paper' }}>
-            {[1, 2, 3].map((value) => (
-              <ListItem
-                key={value}
-                disableGutters
-                secondaryAction={
-                  <IconButton>
-                    <CommentIcon />
-                  </IconButton>
-                }
-              >
-                <ListItemText primary={`ЧАТ ${value}`} />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-        <div className='messageform'>
-        <div className='messageLs'>
-          {messageList.map((message, index) => (<div className={`messageDesign ${message.author==='me'?'me':'bot'}`} key={index}>
-            {message.text}  <sup>{message.author}</sup></div>))}
-        </div>
         
-        <div className='actions'>
-        <TextField
-              style={{ margin: "20px" }}
-              id="outlined-basic"
-              label="Введите сообщение"
-              variant="outlined"
-              value={value} autoFocus
-              onChange={handleChange}
-              
-            />
-          
-        <Button onClick={handleButton} variant="contained" endIcon={<SendIcon />} >
-              Send
-            </Button>
-            </div>
-        </div>
-      </div>
-    </div>
+        <Routes>
+            <Route className='header>' path='/' element={<Layout/>}>
+              <Route index element={<Home/>}/>
+              <Route path='/chats' element={<Chats/>}/>
+              <Route path='/profile' element={<Profile/>}/>
+              <Route path='*' element={<NoChats/>}/>
+            </Route>
+        </Routes>
+      
+   
   );
 }
 
